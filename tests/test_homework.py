e80129e09b45425553b3b465b9f9cbf6923276a4
Paula-HC <paherazoc@unal.edu.co>
2025-06-29 10:02:15 -0500

    Lab 7

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,17 +1,7 @@
-"""Autograding script."""
-
 import os
-
-from homework import pregunta_01 as pregunta
-
+from homework.pregunta_01 import pregunta_01  # <- aquí está el cambio
 
 def test_01():
-    """Test homework"""
-
-    if os.path.exists("files/plots/news.png"):
-        os.remove("files/plots/news.png")
-        os.removedirs("files/plots")
-
-    pregunta.pregunta_01()
-
-    assert os.path.exists("files/plots/news.png")
+    pregunta_01()
+    assert os.path.exists("files/plots/news.png"), "No se generó el archivo news.png"
+  
